18d73bbfc09d3b5886b575a413183517ff365e45
makrandgupta <makrand1996@gmail.com>
2015-11-22 23:58:13 -0500

    Update server.js

--- makrand_website/server.js
+++ makrand_website/server.js
@@ -17,30 +17,23 @@ var configDB = require('./config/database.js');
 
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
-
 require('./config/passport')(passport); // pass passport for configuration
-
-
-
-	// set up our express application
-	app.use(morgan('dev')); // log every request to the console
-	app.use(cookieParser()); // read cookies (needed for auth)
-	// app.use(bodyParser()); // get information from html forms
-	app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-	app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-	// required for passport
-	app.use(session({ 
-		secret: 'ilovescotchscotchyscotchscotch',
-		resave: true,
-		saveUninitialized: true
-	})); // session secret
-	app.use(passport.initialize());
-	app.use(passport.session()); // persistent login sessions
-	app.use(flash()); // use connect-flash for flash messages stored in session
+// required for passport
+app.use(session({ 
+	secret: 'ilovescotchscotchyscotchscotch',
+	resave: true,
+	saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 app.use(express.static(__dirname + '/'));
 
 
