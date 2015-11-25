8d69068f8282deb3c45ac78d0f06d573c382bd2b
Makrand <makrand1996@gmail.com>
2015-11-24 22:02:21 -0500

    completed admin.controller

---
+++ makrand_website/app/api/user/admin.controller.js
@@ -0,0 +1,58 @@
+/**
+ * Created by makrand on 11/24/15.
+ */
+
+'use strict';
+
+var User = require('./../models/user.js');
+var config = require('../../config/environment');
+
+//admin endpoints
+
+//router.get('/admin/edit/:id', isAdmin, adminController.modify);
+exports.editUser = function (req, res) {
+    res.redirect('/edit/'+req.params.id);
+};
+
+//router.get('/delete/:id', isAdmin, adminController.delete);
+exports.delete = function (req, res) {
+        User.remove({
+            _id: req.params.id
+        }, function (err) {
+            if (err)
+            // res.send(err);
+                console.log('deleted');
+            res.redirect('/users');
+        })
+};
+
+//router.get('/make-admin/:id', isAdmin, adminController.makeAdmin);
+exports.makeAdmin = function (req, res) {
+    User.findById(req.params.id, function(err, user){
+        if(err) res.send(err);
+
+        user.local.accountType = 'admin';
+
+        //save the user
+        user.save(function(err) {
+            if(err) res.send(err);
+
+            res.redirect('/users');
+        });
+    });
+};
+
+//router.get('/remove-admin/:id', isAdmin, adminController.removeAdmin);
+exports.removeAdmin = function (req, res) {
+    User.findById(req.params.id, function(err, user){
+        if(err) res.send(err);
+        user.local.accountType = 'user';
+
+        //save the user
+        user.save(function(err) {
+            if(err) res.send(err);
+
+            res.redirect('/users');
+        });
+    });
+};
