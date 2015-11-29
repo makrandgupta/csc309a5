eb006388a3dd49d25647fb2cdd39fb359f5519f1
Makrand Gupta <Makrand Gupta>
2015-11-29 13:50:51 -0500

    added cat model

---
+++ app/api/models/cat.js
@@ -0,0 +1,18 @@
+// app/models/cat.js
+// load the things we need
+var mongoose = require('mongoose');
+
+// define the schema for our user model
+var catSchema = mongoose.Schema({
+	name        : String,
+	age 	    : String,
+	weight  	: String,
+	color 		: String,
+	picture	    : String,
+	breed		: String 
+});
+
+// methods ======================
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Cat', userSchema);
