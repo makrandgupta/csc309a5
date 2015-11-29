26d38f4b382627a8f3fdd8e51be923f157542365
Peter Dodds <peter.sa.d@hotmail.com>
2015-11-29 16:07:57 -0500

    Profile now works, should display cats when they can be added

--- app/api/user/user.controller.js
+++ app/api/user/user.controller.js
@@ -4,6 +4,7 @@
 'use strict';
 
 var User = require('./../models/user.js');
+var Cat = require('./../models/cat.js');
 var request = require('request');
 var multer= require('multer');
 var fs = require('fs');
@@ -28,7 +29,7 @@ exports.allUsers = function (req, res) {
 * */
 
 exports.me = function (req, res) {
-    Cat.find({'_id': { $in: [user.cats]}, function(err, cats) {
+    Cat.find({'_id': { $in: user.cats}}, function(err, cats) {
 	if(err) res.send(err);
 	    
 	res.render('profile.ejs', {
@@ -58,7 +59,7 @@ exports.singleUser = function (req, res) {
     User.findById(req.params.id, function(err, user) {
         if(err) res.send(err);
 	
-	Cat.find({'_id': { $in: [user.cats]}, function(err, cats) {
+	Cat.find({'_id': { $in: user.cats}}, function(err, cats) {
 	    if(err) res.send(err);
 	    
 	    res.render('profile.ejs', {
