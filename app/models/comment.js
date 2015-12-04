d6b69ce659ded7ce07df6685307e171e246348e1
Kevan Hollbach <khollbach@gmail.com>
2015-12-04 16:52:26 -0500

    Started to implement comments.

---
+++ app/models/comment.js
@@ -0,0 +1,17 @@
+// app/models/comment.js
+
+'use strict';
+
+var mongoose = require('mongoose');
+
+// Define the schema for comments.
+var commentSchema = mongoose.Schema({
+	targetUserId : String,
+	sourceUserId : String,
+	sourceUserName : String,
+	text : String,
+});
+
+// Create the model for comments.
+Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment
